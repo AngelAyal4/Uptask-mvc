76c9cbf550f6da9b83c259af321c1b4fbfe13497
AngelAyal4 <ajoseayala.00@gmail.com>
2025-05-16 16:40:01 -0300

    creada la vista para proyeto y seteo en sistema ubuntu

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', '', 'uptask_mvc');
+$db = mysqli_connect('localhost', 'root', 'finoMora2025', 'uptask_mvc');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
